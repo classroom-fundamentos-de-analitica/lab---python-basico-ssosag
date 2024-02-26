05f3eafed99c37a0d3c894c99e655115dd4605b5
Santiago Sosa <ssosag@unal.edu.co>
2024-02-25 22:57:52 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -13,248 +13,225 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 """
 
 
+# Cargar datos
+def cargar_datos(archivo):
+    with open(archivo, "r") as f:
+        datos = [linea.strip().split("\t") for linea in f.readlines()]
+    return datos
+
+
 def pregunta_01():
-    """
-    Retorne la suma de la segunda columna.
+    datos = cargar_datos("data.csv")
+    sumaColumna = 0
 
-    Rta/
-    214
+    for fila in datos:
+        numero = int(fila[1])
+        sumaColumna += numero
 
-    """
-    return
+    return sumaColumna
 
 
 def pregunta_02():
-    """
-    Retorne la cantidad de registros por cada letra de la primera columna como la lista
-    de tuplas (letra, cantidad), ordendas alfabéticamente.
+    datos = cargar_datos("data.csv")
+    registroColumna = {}
+    for fila in datos:
+        registro = fila[0]
+        if registro not in registroColumna:
+            registroColumna[registro] = 1
+        else:
+            registroColumna[registro] += 1
 
-    Rta/
-    [
-        ("A", 8),
-        ("B", 7),
-        ("C", 5),
-        ("D", 6),
-        ("E", 14),
-    ]
+    registroOrdenado = sorted(registroColumna.items())
 
-    """
-    return
+    return registroOrdenado
 
 
 def pregunta_03():
-    """
-    Retorne la suma de la columna 2 por cada letra de la primera columna como una lista
-    de tuplas (letra, suma) ordendas alfabeticamente.
+    datos = cargar_datos("data.csv")
+    registroColumna = {}
+    for fila in datos:
+        registro = fila[0]
+        numero = int(fila[1])
+        if registro not in registroColumna:
+            registroColumna[registro] = numero
+        else:
+            registroColumna[registro] += numero
 
-    Rta/
-    [
-        ("A", 53),
-        ("B", 36),
-        ("C", 27),
-        ("D", 31),
-        ("E", 67),
-    ]
+    registroOrdenado = sorted(registroColumna.items())
 
-    """
-    return
+    return registroOrdenado
 
 
 def pregunta_04():
-    """
-    La columna 3 contiene una fecha en formato `YYYY-MM-DD`. Retorne la cantidad de
-    registros por cada mes, tal como se muestra a continuación.
+    datos = cargar_datos("data.csv")
+    fechasMeses = {}
+    for fila in datos:
+        fecha = fila[2]
+        mes = fecha.split("-")[1]
 
-    Rta/
-    [
-        ("01", 3),
-        ("02", 4),
-        ("03", 2),
-        ("04", 4),
-        ("05", 3),
-        ("06", 3),
-        ("07", 5),
-        ("08", 6),
-        ("09", 3),
-        ("10", 2),
-        ("11", 2),
-        ("12", 3),
-    ]
+        if mes not in fechasMeses:
+            fechasMeses[mes] = 1
+        else:
+            fechasMeses[mes] += 1
 
-    """
-    return
+    mesesOrdenado = sorted(fechasMeses.items())
+
+    return mesesOrdenado
 
 
 def pregunta_05():
-    """
-    Retorne una lista de tuplas con el valor maximo y minimo de la columna 2 por cada
-    letra de la columa 1.
+    datos = cargar_datos("data.csv")
+    registro = {}
+    for fila in datos:
+        letra = fila[0]
+        numero = int(fila[1])
 
-    Rta/
-    [
-        ("A", 9, 2),
-        ("B", 9, 1),
-        ("C", 9, 0),
-        ("D", 8, 3),
-        ("E", 9, 1),
+        if letra not in registro:
+            registro[letra] = {"max": numero, "min": numero}
+        else:
+            registro[letra]["max"] = max(registro[letra]["max"], numero)
+            registro[letra]["min"] = min(registro[letra]["min"], numero)
+
+    resultado = [
+        (letra, registro[letra]["max"], registro[letra]["min"])
+        for letra in sorted(registro)
     ]
-
-    """
-    return
+    return resultado
 
 
 def pregunta_06():
-    """
-    La columna 5 codifica un diccionario donde cada cadena de tres letras corresponde a
-    una clave y el valor despues del caracter `:` corresponde al valor asociado a la
-    clave. Por cada clave, obtenga el valor asociado mas pequeño y el valor asociado mas
-    grande computados sobre todo el archivo.
+    datos = cargar_datos("data.csv")
+    dicResultado = {}
 
-    Rta/
-    [
-        ("aaa", 1, 9),
-        ("bbb", 1, 9),
-        ("ccc", 1, 10),
-        ("ddd", 0, 9),
-        ("eee", 1, 7),
-        ("fff", 0, 9),
-        ("ggg", 3, 10),
-        ("hhh", 0, 9),
-        ("iii", 0, 9),
-        ("jjj", 5, 17),
-    ]
+    for fila in datos:
+        diccionario = fila[4]
+        diccionario = diccionario.split(",")
 
-    """
-    return
+        for elemento in diccionario:
+            elemento = elemento.split(":")
+            clave = elemento[0]
+            valor = int(elemento[1])
+
+            if clave not in dicResultado:
+                dicResultado[clave] = {"min": valor, "max": valor}
+            else:
+                dicResultado[clave]["min"] = min(dicResultado[clave]["min"], valor)
+                dicResultado[clave]["max"] = max(dicResultado[clave]["max"], valor)
+
+        resultado = [
+            (clave, dicResultado[clave]["min"], dicResultado[clave]["max"])
+            for clave in sorted(dicResultado)
+        ]
+
+    return resultado
 
 
 def pregunta_07():
-    """
-    Retorne una lista de tuplas que asocien las columnas 0 y 1. Cada tupla contiene un
-    valor posible de la columna 2 y una lista con todas las letras asociadas (columna 1)
-    a dicho valor de la columna 2.
+    datos = cargar_datos("data.csv")
+    dicRegistros = {}
 
-    Rta/
-    [
-        (0, ["C"]),
-        (1, ["E", "B", "E"]),
-        (2, ["A", "E"]),
-        (3, ["A", "B", "D", "E", "E", "D"]),
-        (4, ["E", "B"]),
-        (5, ["B", "C", "D", "D", "E", "E", "E"]),
-        (6, ["C", "E", "A", "B"]),
-        (7, ["A", "C", "E", "D"]),
-        (8, ["E", "D", "E", "A", "B"]),
-        (9, ["A", "B", "E", "A", "A", "C"]),
-    ]
+    for fila in datos:
+        letra = fila[0]
+        numero = int(fila[1])
+        if numero not in dicRegistros:
+            dicRegistros[numero] = [letra]
+        else:
+            dicRegistros[numero].append(letra)
 
-    """
-    return
+    resultado = sorted(dicRegistros.items())
+
+    return resultado
 
 
 def pregunta_08():
-    """
-    Genere una lista de tuplas, donde el primer elemento de cada tupla contiene  el valor
-    de la segunda columna; la segunda parte de la tupla es una lista con las letras
-    (ordenadas y sin repetir letra) de la primera  columna que aparecen asociadas a dicho
-    valor de la segunda columna.
+    datos = cargar_datos("data.csv")
+    dicRegistros = {}
 
-    Rta/
-    [
-        (0, ["C"]),
-        (1, ["B", "E"]),
-        (2, ["A", "E"]),
-        (3, ["A", "B", "D", "E"]),
-        (4, ["B", "E"]),
-        (5, ["B", "C", "D", "E"]),
-        (6, ["A", "B", "C", "E"]),
-        (7, ["A", "C", "D", "E"]),
-        (8, ["A", "B", "D", "E"]),
-        (9, ["A", "B", "C", "E"]),
+    for fila in datos:
+        letra = fila[0]
+        numero = int(fila[1])
+        if numero not in dicRegistros:
+            dicRegistros[numero] = set()
+        dicRegistros[numero].add(letra)
+
+    resultado = [
+        (numero, sorted(letras)) for numero, letras in sorted(dicRegistros.items())
     ]
 
-    """
-    return
+    return resultado
 
 
 def pregunta_09():
-    """
-    Retorne un diccionario que contenga la cantidad de registros en que aparece cada
-    clave de la columna 5.
+    datos = cargar_datos("data.csv")
+    dicResultado = {}
 
-    Rta/
-    {
-        "aaa": 13,
-        "bbb": 16,
-        "ccc": 23,
-        "ddd": 23,
-        "eee": 15,
-        "fff": 20,
-        "ggg": 13,
-        "hhh": 16,
-        "iii": 18,
-        "jjj": 18,
-    }
+    for fila in datos:
+        diccionario = fila[4]
+        diccionario = diccionario.split(",")
 
-    """
-    return
+        for elemento in diccionario:
+            elemento = elemento.split(":")
+            clave = elemento[0]
+
+            if clave not in dicResultado:
+                dicResultado[clave] = 1
+            else:
+                dicResultado[clave] += 1
+
+    resultado = dict(sorted(dicResultado.items()))
+
+    return resultado
 
 
 def pregunta_10():
-    """
-    Retorne una lista de tuplas contengan por cada tupla, la letra de la columna 1 y la
-    cantidad de elementos de las columnas 4 y 5.
+    datos = cargar_datos("data.csv")
+    listaResultado = []
 
-    Rta/
-    [
-        ("E", 3, 5),
-        ("A", 3, 4),
-        ("B", 4, 4),
-        ...
-        ("C", 4, 3),
-        ("E", 2, 3),
-        ("E", 3, 3),
-    ]
+    for fila in datos:
+        letra = fila[0]
+        elementosCol4 = len(fila[3].split(","))
+        elementosCol5 = len(fila[4].split(","))
+        listaResultado.append((letra, elementosCol4, elementosCol5))
 
-
-    """
-    return
+    return listaResultado
 
 
 def pregunta_11():
-    """
-    Retorne un diccionario que contengan la suma de la columna 2 para cada letra de la
-    columna 4, ordenadas alfabeticamente.
+    datos = cargar_datos("data.csv")
+    dicResultado = {}
 
-    Rta/
-    {
-        "a": 122,
-        "b": 49,
-        "c": 91,
-        "d": 73,
-        "e": 86,
-        "f": 134,
-        "g": 35,
-    }
+    for fila in datos:
+        numero = int(fila[1])
+        letras = fila[3].split(",")
+        for letra in letras:
+            if letra not in dicResultado:
+                dicResultado[letra] = numero
+            else:
+                dicResultado[letra] += numero
 
+    resultado = dict(sorted(dicResultado.items()))
 
-    """
-    return
+    return resultado
 
 
 def pregunta_12():
-    """
-    Genere un diccionario que contengan como clave la columna 1 y como valor la suma de
-    los valores de la columna 5 sobre todo el archivo.
+    datos = cargar_datos("data.csv")
+    dicResultado = {}
+    for fila in datos:
+        sumaValores = 0
+        letra = fila[0]
+        diccionario = fila[4]
+        diccionario = diccionario.split(",")
 
-    Rta/
-    {
-        'A': 177,
-        'B': 187,
-        'C': 114,
-        'D': 136,
-        'E': 324
-    }
+        for item in diccionario:
+            item = item.split(":")
+            sumaValores += int(item[1])
 
-    """
-    return
+        if letra not in dicResultado:
+            dicResultado[letra] = sumaValores
+        else:
+            dicResultado[letra] += sumaValores
+
+        resultado = dict(sorted(dicResultado.items()))
+
+    return resultado
